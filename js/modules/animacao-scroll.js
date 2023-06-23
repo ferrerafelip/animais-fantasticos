6ef76004d0ea603cb24bb11363ca7360bdd3da4d
Felipe Ferreira <ferreirafelip13@gmail.com>
2023-06-23 20:42:37 -0300

    animação-scroll refeita

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,26 +1,44 @@
-export default class ScrollAnima{
-  constructor(sections){
+export default class ScrollAnima {
+  constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this)
-  }
-  
-  animaScroll(){
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = (sectionTop - this.windowMetade) < 0;
-      if(sectionVisible){
-        section.classList.add('ativo');
-      }
-      else if(section.classList.contains('ativo')){
-        section.classList.remove('ativo');
-      }
-    })
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  init(){
-    window.addEventListener('scroll', this.animaScroll);
+  // Pega a distancia de cada item em relação ao top do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: offset - this.windowMetade,
+      };
+    });
+  }
+
+  // Verifica a disatncia de um objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+  // Remove o evento de scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
-  
